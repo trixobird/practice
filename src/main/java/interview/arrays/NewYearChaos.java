c211b26f82376cb30eef0c650d7772c47e9011e2
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2019-10-06 19:06:13 +0300

    Make NewYearChaos Recursive

--- src/main/java/interview/arrays/NewYearChaos.java
+++ src/main/java/interview/arrays/NewYearChaos.java
@@ -23,29 +23,30 @@ class NewYearChaos {
             }
 
             if (i < q.length - 1 && q[i] - q[i + 1] > 1) {
-                int tmpIdx = i;
-                while (tmpIdx < q.length - 1 && q[tmpIdx] > q[tmpIdx + 1]) {
-                    swap(q, tmpIdx, tmpIdx + 1);
-                    bribes++;
-                    tmpIdx++;
-                }
+                bribes = swap(q, i, i + 1, bribes);
             }
 
             if (i > 0 && q[i] - q[i - 1] < 1) {
-                int tmpIdx = i;
-                while (tmpIdx > 0 && q[tmpIdx] < q[tmpIdx - 1]) {
-                   swap(q, tmpIdx, tmpIdx - 1);
-                    bribes++;
-                    tmpIdx--;
-                }
+               bribes = swap(q, i, i - 1, bribes);
             }
         }
         return bribes;
     }
 
-    private static void swap(int[] q, int a, int b) {
+    private static int swap(int[] q, int a, int b, int bribes) {
+        bribes++;
+
         int tmp = q[b];
         q[b] = q[a];
         q[a] = tmp;
+
+        if (b < q.length - 1 && q[b] > q[b + 1]) {
+            bribes = swap(q, b, b + 1, bribes);
+        }
+
+        if (a > 0 && q[a] < q[a - 1]) {
+            bribes = swap(q, a - 1, a, bribes);
+        }
+        return bribes;
     }
 }
