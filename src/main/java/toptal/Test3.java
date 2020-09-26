71d27a4ca25927707735b54bceee0c41ed3f9473
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2020-09-26 14:55:29 +0300

    Toptal interview

---
+++ src/main/java/toptal/Test3.java
@@ -0,0 +1,22 @@
+package toptal;
+
+public class Test3 {
+}
+
+class Main3 {
+    public static void main(String[] args) {
+
+    }
+
+//    Example test:   'babaa'
+//    WRONG ANSWER (got 0 expected 2)
+//
+//    Example test:   'ababa'
+//    WRONG ANSWER (got 0 expected 4)
+//
+//    Example test:   'aba'
+//    OK
+//
+//    Example test:   'bbbbb'
+//    WRONG ANSWER (got 0 expected 6)
+}
